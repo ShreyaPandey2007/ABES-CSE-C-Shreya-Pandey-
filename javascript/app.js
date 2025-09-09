af99e321ef5e9fdbb8a9463fbb4a89b8c0d8d545
ShreyaPandey2007 <shreya912pandey@gmail.com>
2025-09-09 18:53:58 +0530

    new

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,14 @@
-console.error("oops! something went wrong");
-var x="hi";
-var y="hello";
-console.log(x+" "+y);
-console.warn()
+let x=10;
+let y=20;
+let z=30;
+if((x<y && x>z)||(x>y && x<z)){
+    console.log(x);
+
+}
+else if((y<x && y>z)||(y<z && y>x)){
+    console.log(y);
+}
+else  if((z<x && z>y)||(z<y && z>x)){
+    console.log(z);
+
+}
