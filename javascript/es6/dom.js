b3f5efa6dc54fc6f6b1ab84200b7016a6f280bda
ShreyaPandey2007 <shreya912pandey@gmail.com>
2025-10-28 16:10:16 +0530

    initial commit

---
+++ javascript/es6/dom.js
@@ -0,0 +1,7 @@
+const div=document.createElement("div")
+const h1=document.createElement("h1")
+h1.innerText="hi"
+div.appendChild(h1)
+const ele= document.getElementById("root")
+ele.appendChild(div)
+
