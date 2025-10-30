a1302eebfd78b6118c00d00d671a744e1a953dd8
ShreyaPandey2007 <shreya912pandey@gmail.com>
2025-10-30 09:33:36 +0530

    initial

---
+++ frontend/shree/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+ import './Card.css'
+const Card = () => {
+  return (
+    <div id="card">
+        <h1>hi there</h1>
+        <img src="https://tse1.mm.bing.net/th/id/OIP.2SvXabEg_hng8k3yaxDtSQHaHG?pid=Api&P=0&h=180" alt="" height={100} width={100} />
+    </div>
+  )
+}
+
+export default Card
