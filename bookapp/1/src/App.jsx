5401d7942e97cc3d6c4d9a65209328fa48f8e8b7
ShreyaPandey2007 <shreya912pandey@gmail.com>
2025-11-04 16:04:12 +0530

    initial

---
+++ bookapp/1/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+import Book from './Book'
+
+function App() {
+  
+
+  return (
+    <>
+    <div id="shree">
+       <Book title="Physice" price="255" img='https://tse4.mm.bing.net/th/id/OIP.vL1xAhbqStT0VNcXgsARuAHaJB?pid=Api&P=0&h=180'/>
+       <Book title="Math" price="255" img='https://tse1.mm.bing.net/th/id/OIP.OGDJZetwwtBho5rT71TjygHaKC?pid=Api&P=0&h=180'/>
+       <Book title="Chemistry" price="255" img='https://tse3.mm.bing.net/th/id/OIP.2QCYRXrh3x1hjEMOChIwnAHaJ-?pid=Api&P=0&h=180'/>
+
+    </div>
+   
+      
+    </>
+  )
+}
+
+export default App
---
+++ bookapp/1/src/Book.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Book.css'
+const Book = (props) => {
+    
+  return (
+    <div id="book">
+        <img src={props.img} alt="" height={100} width={100}/>
+        <h3>{props.title}</h3>
+        <h3>{props.price}</h3>
+        <button></button>
+        but
+    </div>
+  )
+}
+
+export default Book
